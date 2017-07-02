67f2929c6110287d7df7a41244c84616ad941a48
olanter <theolanter@gmail.com>
2017-07-03 02:32:28 +0700

    add mSG

--- test.php
+++ test.php
@@ -9,13 +9,16 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('IuM1APpXsYJiNcxSom3dxgpTGQj6A/gRFF1crr+uuGB6HqM7a4g2SKgX91zMC3TKHNLJ9NqhDN7/1FTKfhmJ6K3TOG2srD5uby0WFgydq5w9g6F55c6VlpuU7oqDlGb3cFNhLW4f+7Ju/3vx2uMyKAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2fa416dd228a8bc1a9ceaa51fc768f48']);
 
+echo "Start<br>";
 
 $response = $bot->getMessageContent('6328682950755');
 if ($response->isSucceeded()) {
+	echo "GetMSG<br>";
     $tempfile = tmpfile();
     fwrite($tempfile, $response->getRawBody());
 	echo "Succeeded<br>";
 } else {
+	echo "FAIL<br>";
     error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
 }
 
