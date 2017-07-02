bd65fe8d7f5bad8ad3eca62cb7014b93433cfa67
olanter <theolanter@gmail.com>
2017-07-03 02:43:13 +0700

    add vardump2

--- test.php
+++ test.php
@@ -16,7 +16,7 @@ if ($response->isSucceeded()) {
 	echo "GetMSG<br>";
     $tempfile = tmpfile();
     fwrite($tempfile, $response->getRawBody());
-	var_dump($response->getRawBody());
+	var_dump($response);
 	echo "Succeeded<br>";
 } else {
 	echo "FAIL<br>";
